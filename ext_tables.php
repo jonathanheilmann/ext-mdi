f73990f0bd8c61a5745e84f53465badd3b5f75b8
jonathanheilmann <mail@jonathan-heilmann.de>
2016-05-10 13:25:36 +0200

    [TASK] Clean up ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,9 @@
 <?php
-if (!defined('TYPO3_MODE')) {
+if (!defined('TYPO3_MODE'))
 	die('Access denied.');
-}
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-		$_EXTKEY,
+		'Heilmann.' . $_EXTKEY,
 		'Mdi',
 		'Material design icons'
 );
@@ -20,48 +19,22 @@ if (TYPO3_MODE === 'BE') {
 			'mdi',    // Submodule key
 			'',                        // Position
 			array(
-					'Mdi' => 'list'
+				'Mdi' => 'list'
 			),
 			array(
-					'access' => 'user,group',
-					'icon'   => 'EXT:mdi/Resources/Public/Icons/action/ic_android_black_18dp.png',
-					'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mdi.xlf',
+				'access' => 'user,group',
+				'icon'   => 'EXT:mdi/Resources/Public/Icons/action/ic_android_black_18dp.png',
+				'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mdi.xlf',
 			)
 	);
 }
 
 // Add icons (24dp)
-addIconSpriteForIconSet($_EXTKEY, 'action');
-addIconSpriteForIconSet($_EXTKEY, 'alert');
-addIconSpriteForIconSet($_EXTKEY, 'av');
-addIconSpriteForIconSet($_EXTKEY, 'communication');
-addIconSpriteForIconSet($_EXTKEY, 'content');
-addIconSpriteForIconSet($_EXTKEY, 'device');
-addIconSpriteForIconSet($_EXTKEY, 'editor');
-addIconSpriteForIconSet($_EXTKEY, 'file');
-addIconSpriteForIconSet($_EXTKEY, 'hardware');
-addIconSpriteForIconSet($_EXTKEY, 'image');
-addIconSpriteForIconSet($_EXTKEY, 'maps');
-addIconSpriteForIconSet($_EXTKEY, 'navigation');
-addIconSpriteForIconSet($_EXTKEY, 'notification');
-addIconSpriteForIconSet($_EXTKEY, 'places');
-addIconSpriteForIconSet($_EXTKEY, 'social');
-addIconSpriteForIconSet($_EXTKEY, 'toggle');
-
-// Add icons (16dp)
-addIconSpriteForIconSet($_EXTKEY, 'action16dp');
-addIconSpriteForIconSet($_EXTKEY, 'content16dp');
-addIconSpriteForIconSet($_EXTKEY, 'navigation16dp');
-addIconSpriteForIconSet($_EXTKEY, 'toggle16dp');
-
-/**
- *
- *
- * @param $iconSet
- */
-function addIconSpriteForIconSet($extKey, $iconSet) {
+$iconSets = array('action', 'alert', 'av', 'communication', 'content', 'device', 'editor', 'file', 'hardware', 'image', 'maps', 'navigation', 'notification', 'places', 'social', 'toggle', 'action16dp', 'content16dp', 'navigation16dp', 'toggle16dp');
+foreach ($iconSets as $iconSet)
+{
 	$cssPath = 'Resources/Public/Sprites/' . $iconSet . '/stylesheet.css';
-	$cssFileContents = file_get_contents(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($extKey) . $cssPath);
+	$cssFileContents = file_get_contents(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . $cssPath);
 	preg_match_all('/(\.?'.addcslashes('t3-icon-mdi_' . $iconSet, '-').'.*?)\s?\{/', $cssFileContents, $matches);
 	$classList = $matches[1];
 	$icons = array();
@@ -70,7 +43,7 @@ function addIconSpriteForIconSet($extKey, $iconSet) {
 	}
 
 	\TYPO3\CMS\Backend\Sprite\SpriteManager::addIconSprite(
-			$icons,
-			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::siteRelPath($extKey) . $cssPath
+		$icons,
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::siteRelPath($_EXTKEY) . $cssPath
 	);
 }
